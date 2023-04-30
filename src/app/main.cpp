939ff00e622b6551189099b0d826b2b228faa2cf
SokolovskiyNick <nikolael.pochta@gmail.com>
2023-05-01 00:29:22 +0700

    Fix mistakes of reading, #12

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -7,48 +7,50 @@
 
 using namespace std;
 
-int main(int argc, char** argv)
+int main(int argc, char* argv[])
 {
     srand(time(NULL));
-    bool numbersOnly;
-    bool lowRegistrOnly;
-    bool upRegistrOnly;
-    bool upLowRegistr;
-    int passwordLength;
+    bool numbersOnly = false, lowRegistrOnly = false, upRegistrOnly = false,
+         upLowRegistr = false, symbols = false;
+    int passwordLength = 0;
     string password;
     if (argc >= 2)
-        passwordLength = stoi(argv[2]);
-
-    if (argc >= 3 && string(argv[3]) == "-0") 
-        numbersOnly = true;
-    
-    if (argc >= 3 && string(argv[3]) == "-A") 
-        upRegistrOnly = true;
-
-    if (argc >= 3 && string(argv[3]) == "-a")
-        lowRegistrOnly = true;
-
-    if (argc >= 3 && string(argv[3]) == "-a") 
-        upLowRegistr = true;
-    
+        passwordLength = atoi(argv[1]);
+    if (argc >= 3) {
+        if (string(argv[2]) == "-0")
+            numbersOnly = true;
+        if (string(argv[2]) == "-A")
+            upRegistrOnly = true;
+        if (string(argv[2]) == "-a")
+            lowRegistrOnly = true;
+        if (string(argv[2]) == "-Aa")
+            upLowRegistr = true;
+        if (string(argv[2]) == "-symbols")
+            symbols = true;
+    }
     if (numbersOnly) {
         for (int i = 0; i < passwordLength; i++)
             password += getRandChar('0', '9');
-    }  
-    
+    }
+
     else if (upRegistrOnly) {
         for (int i = 0; i < passwordLength; i++)
-            password += getRandChar('0', '9');
+            password += getRandChar('A', 'Z');
     }
 
     else if (lowRegistrOnly) {
         for (int i = 0; i < passwordLength; i++)
-            password += getRandChar('0', '9');
+            password += getRandChar('a', 'z');
     }
 
     else if (upLowRegistr) {
         for (int i = 0; i < passwordLength; i++)
-            password += getRandChar('0', '9');
+            password += genAa();
+    }
+
+    else if (symbols) {
+        for (int i = 0; i < passwordLength; i++)
+            password += genSym();
     }
     cout << "Password: " << password << endl;
     return 0;
