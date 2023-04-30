a91c68d135c15b7b7efa2eccecdf2a477bab2327
Hoz <zh.seva2004@mail.ru>
2023-04-30 21:37:26 +0700

    Add tests, #11

--- test/test.cpp
+++ test/test.cpp
@@ -1,20 +1,20 @@
-#include <ctest.h>
 #include <cstdlib>
+#include <ctest.h>
 
 #include "libapp/pwgen.h"
 
 #define N 28
 
-CTEST (check_generation, check_numbers)
+CTEST(check_generation, check_numbers)
 {
     char real = getRandChar('0', '9');
-    int left = (int) '0';
-    int right = (int) '9';
+    int left = (int)'0';
+    int right = (int)'9';
     ASSERT_INTERVAL(left, right, real);
 }
 
-
-CTEST(check_generation, check_symbol) {
+CTEST(check_generation, check_symbol)
+{
     char* string = new char[N];
     int k = 0;
     for (char j = '!'; j <= '/'; j++) {
@@ -28,8 +28,47 @@ CTEST(check_generation, check_symbol) {
     }
     int real = 0;
     int x = genSym();
-    for(int i = 0; i < N; i++) {
-        if(x == string[i]) {
+    for (int i = 0; i < N; i++) {
+        if (x == string[i]) {
+            real = 1;
+            break;
+        }
+    }
+    const int expected = 1;
+    delete[] string;
+    ASSERT_EQUAL(expected, real);
+}
+
+CTEST(check_generation, check_upper_case)
+{
+    char real = getRandChar('A', 'Z');
+    int left = (int)'A';
+    int right = (int)'Z';
+    ASSERT_INTERVAL(left, right, real);
+}
+
+CTEST(check_generation, check_lower_case)
+{
+    char real = getRandChar('a', 'z');
+    int left = (int)'a';
+    int right = (int)'z';
+    ASSERT_INTERVAL(left, right, real);
+}
+
+CTEST(check_generation, check_up_and_low)
+{
+    char* string = new char[N];
+    int k = 0;
+    for (char j = 'A'; j <= 'Z'; j++) {
+        string[k++] = j;
+    }
+    for (char j = 'a'; j <= 'z'; j++) {
+        string[k++] = j;
+    }
+    int real = 0;
+    int x = genAa();
+    for (int i = 0; i < N; i++) {
+        if (x == string[i]) {
             real = 1;
             break;
         }
