3066adc4a01c1b57f228d2147cd66306499b2aaf
SokolovskiyNick <nikolael.pochta@gmail.com>
2023-04-30 19:47:14 +0700

    Add test for func gensym, #9

---
+++ test/test.cpp
@@ -0,0 +1,28 @@
+#include <ctest.h>
+
+#include "libapp/pwgen.h"
+#define N 28
+
+CTEST(symbol, check_ok) {
+    char* string = new char[N];
+    int k = 0;
+    for (char j = '!'; j <= '/'; j++) {
+        string[k++] = j;
+    }
+    for (char j = ':'; j <= '@'; j++) {
+        string[k++] = j;
+    }
+    for (char j = '['; j <= '`'; j++) {
+        string[k++] = j;
+    }
+    int real = 0;
+    int x = genSym();
+    for(int i = 0; i < N; i++) {
+        if(x == string[i]) {
+            real = 1;
+            break;
+        }
+    }
+    const int expected = 1;
+    ASSERT_EQUAL(expected, real);
+}
