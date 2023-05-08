aef2a74bb19932ae364849b2422a8b81895226fb
DanielTernovskiy <danielternovski@gmail.com>
2023-05-08 17:28:03 +0700

    Add counting sort for arguments, #15

---
+++ src/libapp/sort.cpp
@@ -0,0 +1,36 @@
+#include <cstdlib>
+#include <string.h>
+
+#include "macr.h"
+
+char **countingSort (char** arr, int oldSize, int *newSize) {
+
+    int size = 0;
+    int count = 0;
+    int index = 0;
+    char **args =  (char**) malloc (sizeof(char*));
+    for (int i = 0; i < oldSize; i++) {
+        char flag = 0;
+        for (int j = 0; j < oldSize; j++) {
+        	if (i == j) {
+        		break;
+        	}
+                
+        	if ((strcmp(arr[i], arr[j])) == 0) {
+        		flag = 1;
+                break;
+        	}
+        }
+            
+        if (!flag ) {
+            args =  (char**) realloc (args, (++size) * sizeof(char*));
+            args[index] = (char*) malloc (strlen(arr[i]) * sizeof(char));
+            strcpy(args[index], arr[i]);
+            index += 1;
+            count += 1;
+        }
+    }
+    
+    *(newSize) = count; 
+    return args;
+}
