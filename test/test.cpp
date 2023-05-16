8c346365702d053022deb694d27c9d1c9160db88
DanielTernovskiy <danielternovski@gmail.com>
2023-05-16 19:46:17 +0700

    Add test for function countingSort(), #23

--- test/test.cpp
+++ test/test.cpp
@@ -1,9 +1,13 @@
 #include <cstdlib>
 #include <ctest.h>
+#include <cstring>
 
 #include "libapp/pwgen.h"
+#include "libapp/sort.h"
 
 #define N 28
+
+using namespace std;
 
 CTEST(check_generation, check_numbers)
 {
@@ -75,5 +79,39 @@ CTEST(check_generation, check_up_and_low)
     }
     const int expected = 1;
     delete[] string;
+    ASSERT_EQUAL(expected, real);
+}
+
+CTEST (check_arguments, check_sort) 
+{
+    int expected = 0, real = 0, idx = 0, size = 0;
+    const char* arr[] = {"-a", "-A", "-A", "-0", "--symbols", "-a"};
+    const char* sortArr[] = {"-a", "-A", "-0", "--symbols"};
+    int lenArgs = sizeof(arr) / sizeof(arr[0]);
+    int lenSortArgs = sizeof(sortArr) / sizeof(sortArr[0]);
+    char** args = (char**)malloc(sizeof(char*));
+    for (int i = 0; i < lenArgs; i++) {
+        args = (char**)realloc(args, (++size) * sizeof(char*));
+        args[idx] = (char*)malloc(strlen(arr[i]) * sizeof(char));
+        strcpy(args[idx], arr[i]);
+        idx += 1;
+    }
+    int newSize = 0;
+    int check[lenSortArgs];
+    char** sortArgs = countingSort(args, lenArgs, &newSize);
+    for (int i = 0; i < newSize; i++) {
+        if (strcmp(sortArr[i], sortArgs[i]) == 0) {
+            check[i] = 0;
+        }
+    }
+    for (int i = 0; i < lenSortArgs; i++) {
+        if (check[i] != 0) {
+            real = 1;
+        }
+    }
+    for (int i = 0; i < lenArgs; i++) {
+        free(args[i]);
+    }
+    free(args);
     ASSERT_EQUAL(expected, real);
 }
